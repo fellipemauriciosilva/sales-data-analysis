c7bc3c4a25d6434cf33a004d0cc8a1063f53c210
FELLIPE MAURICIO DA SILVA <4024355@riachuelo.com.br>
2020-11-18 16:36:36 -0300

    incluindo estrutura base

---
+++ sales-data-analysis-batch/src/main/java/br/com/agibank/batch/SalesDataAnalysisBatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.agibank.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesDataAnalysisBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesDataAnalysisBatchApplication.class, args);
+	}
+
+}
